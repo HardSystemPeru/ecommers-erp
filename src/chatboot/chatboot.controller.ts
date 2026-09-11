31b024c80e1d9c62dc2993b96c544b3933caf1b9
carlosRojas2001 <crojas@hsperu.pe>
2026-09-11 14:56:47 -0500

    final

--- src/chatboot/chatboot.controller.ts
+++ src/chatboot/chatboot.controller.ts
@@ -1,22 +1,22 @@
 import { Controller, Post, Body, Query, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { Throttle } from '@nestjs/throttler';
 import { Chat } from './chat.service'; 
-
+import { ChatbootService } from './chatboot.service';
  
 @Controller('chatboot')
 export class ChatbootController {
   constructor(
-    private readonly chatService: Chat) {}
+    private readonly chatService: ChatbootService,) {}
 
-//  @Get('ver-mas/:consultaId')
-//   async consult(@Param('consultaId') consultaId: string, @Query('pagina', ParseIntPipe) pagina: number,){
-//     return this.chatService.verMas(consultaId,pagina)
-//   }
+ @Get('ver-mas/:consultaId')
+  async consult(@Param('consultaId') consultaId: string, @Query('pagina', ParseIntPipe) pagina: number,){
+    return this.chatService.verMas(consultaId,pagina)
+  }
 
   @Post()
   @Throttle({ default: { limit: 30, ttl: 60000 } }) 
   async chat( @Body('message') message: string, ) {
-    return this.chatService.verMas(message,10);
+    return this.chatService.chat(message);
   }
 
 }
